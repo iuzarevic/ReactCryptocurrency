9a24efb43ae2bcf08dd34621221449a31014ef9d
Ivan Užarević <ivan.uzarevic1995@gmail.com>
2019-04-27 06:42:20 +0200

    Fixing syntax errors

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Search from './Search';
 import { Link } from 'react-router-dom';
-import logo from './logo.png'
+import logo from './logo.png';
 import './Header.css';
 
 const Header = () => {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/common/Header';
-import List from './components/list/List'
-import './index.css'
+import List from './components/list/List';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Notfound from './components/notfound/Notfound';
 import Detail from './components/detail/Detail';
+import './index.css';
 
 
 const App = () => {
@@ -13,10 +13,10 @@ const App = () => {
         <BrowserRouter>
             <div>
                 <Header />
+
                 <Switch>
                     <Route path="/" component={List} exact />
-                    {/*This is dynamic path in Router*/}
-                    <Route path="/currency/:id"component={Detail} exact/>
+                    <Route path="/currency/:id" component={Detail} exact/>
                     <Route component={Notfound}/>
                 </Switch>
             </div>
